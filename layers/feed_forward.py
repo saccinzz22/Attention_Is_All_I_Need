df2046d8447062bd1aa25cf841aac98ec16a73f5
saccinzz22 <sachinnair891@gmail.com>
2025-03-13 13:48:04 +0530

    Create feed_forward.py

---
+++ layers/feed_forward.py
@@ -0,0 +1,9 @@
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout):
+        super().__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.fc2(self.dropout(torch.nn.functional.gelu(self.fc1(x))))
