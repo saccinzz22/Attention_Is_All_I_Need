7af7ba70b9df07a6c91a4a2fe2d47ca8af436371
saccinzz22 <sachinnair891@gmail.com>
2025-03-13 13:52:11 +0530

    Create decoder_layer.py

---
+++ blocks/decoder_layer.py
@@ -0,0 +1,16 @@
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.cross_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = FeedForward(d_model, d_ff, dropout)
+        self.norm1 = LayerNormalization(d_model)
+        self.norm2 = LayerNormalization(d_model)
+        self.norm3 = LayerNormalization(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, enc_output, src_mask, tgt_mask):
+        x = self.norm1(x + self.dropout(self.self_attn(x, x, x, tgt_mask)))
+        x = self.norm2(x + self.dropout(self.cross_attn(x, enc_output, enc_output, src_mask)))
+        x = self.norm3(x + self.dropout(self.feed_forward(x)))
+        return x
