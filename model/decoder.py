1d7d35ceb6caa8bc1b19c81da182298b841be40d
saccinzz22 <sachinnair891@gmail.com>
2025-03-13 13:44:45 +0530

    Create decoder.py

---
+++ model/decoder.py
@@ -0,0 +1,12 @@
+class Decoder(nn.Module):
+    def __init__(self, num_layers, d_model, num_heads, d_ff, dropout):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            DecoderLayer(d_model, num_heads, d_ff, dropout)
+            for _ in range(num_layers)
+        ])
+
+    def forward(self, x, enc_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, enc_output, src_mask, tgt_mask)
+        return x
