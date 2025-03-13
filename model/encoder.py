e9d902934c01a81495b967dc145a69e08f59bd2c
saccinzz22 <sachinnair891@gmail.com>
2025-03-13 13:42:15 +0530

    Update encoder.py

--- model/encoder.py
+++ model/encoder.py
@@ -1,1 +1,12 @@
+class Encoder(nn.Module):
+    def __init__(self, num_layers, d_model, num_heads, d_ff, dropout):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            EncoderLayer(d_model, num_heads, d_ff, dropout)
+            for _ in range(num_layers)
+        ])
 
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return x
