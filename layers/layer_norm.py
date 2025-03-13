6683992037ff8f240d808b28bf8c63539fc8ab87
saccinzz22 <sachinnair891@gmail.com>
2025-03-13 13:48:36 +0530

    Create layer_norm.py

---
+++ layers/layer_norm.py
@@ -0,0 +1,11 @@
+class LayerNormalization(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super().__init__()
+        self.eps = eps
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+    
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        return self.gamma * (x - mean) / torch.sqrt(var + self.eps) + self.beta
