21e1824b8c48f6949bab7ff7afea531a6ab0df00
saccinzz22 <sachinnair891@gmail.com>
2025-03-14 15:55:21 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,7 @@
+import os
+import torch
+import torch.optim as optim
+import tiktoken
 from model import Transformer
 
 tokenizer = tiktoken.get_encoding("gpt2")
@@ -5,7 +9,7 @@ tokenizer = tiktoken.get_encoding("gpt2")
 device = "cuda" if torch.cuda.is_available() else "mps" if hasattr(torch.backends, "mps") and torch.backends.mps.is_available() else "cpu"
 print(f"Using device: {device}")
 
-data_path = os.path.join("data", "shakespeare", "input.txt")
+data_path =" /Users/sachin/Documents/input/txt"
 with open(data_path, "r") as f:
     text = f.read()
 
@@ -26,11 +30,10 @@ y = buf[1:].view(B, T)
 src_mask = tgt_mask = None  
 
 model = Transformer(num_layers, d_model, num_heads, d_ff, vocab_size, dropout).to(device)
-
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-epochs = 3
 
+epochs = 3
 for epoch in range(epochs):
     model.train()
     optimizer.zero_grad()
