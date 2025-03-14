06840d71710aac6fb4ba3939afc4c6c0a77eabd4
saccinzz22 <sachinnair891@gmail.com>
2025-03-14 16:01:27 +0530

    Update token.py

--- embeddings/token.py
+++ embeddings/token.py
@@ -1,1 +1,13 @@
+import torch
+import torch.nn as nn
+import math  
+
+class TokenEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.d_model = d_model
+        
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
 
