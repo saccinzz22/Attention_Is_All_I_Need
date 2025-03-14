b9ed03d5c515f35143f143efad1116a446eba298
saccinzz22 <sachinnair891@gmail.com>
2025-03-14 15:48:56 +0530

    Update decoder.py

--- model/decoder.py
+++ model/decoder.py
@@ -1,15 +1,15 @@
 import torch.nn as nn
-from src.blocks.encoder_layer import EncoderLayer
+from src.blocks.decoder_layer import DecoderLayer
 
-class Encoder(nn.Module):
+class Decoder(nn.Module):
     def __init__(self, num_layers, d_model, num_heads, d_ff, dropout):
         super().__init__()
         self.layers = nn.ModuleList([
-            EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)
+            DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)
         ])
         self.norm = nn.LayerNorm(d_model)
 
-    def forward(self, x, mask):
+    def forward(self, x, enc_output, src_mask, tgt_mask):
         for layer in self.layers:
-            x = layer(x, mask)
+            x = layer(x, enc_output, src_mask, tgt_mask)
         return self.norm(x)
