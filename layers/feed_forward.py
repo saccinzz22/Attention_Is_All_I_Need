14e911777dd7042b62ce6b13be9e452801094995
saccinzz22 <sachinnair891@gmail.com>
2025-03-14 15:46:00 +0530

    Update feed_forward.py

--- layers/feed_forward.py
+++ layers/feed_forward.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 class FeedForward(nn.Module):
-    def __init__(self, d_model, d_ff, dropout):
+    def __init__(self, d_model, d_ff, dropout=0.1):
         super().__init__()
         self.fc1 = nn.Linear(d_model, d_ff)
         self.fc2 = nn.Linear(d_ff, d_model)
         self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        return self.fc2(self.dropout(F.gelu(self.fc1(x))))
+        return self.fc2(self.dropout(self.relu(self.fc1(x))))
