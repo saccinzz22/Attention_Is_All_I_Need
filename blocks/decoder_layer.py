e3a2ffe29eb3621fd461c5f0d0b4dc1175b88762
saccinzz22 <sachinnair891@gmail.com>
2025-03-14 15:17:42 +0530

    Update decoder_layer.py

--- blocks/decoder_layer.py
+++ blocks/decoder_layer.py
@@ -1,16 +1,19 @@
 class DecoderLayer(nn.Module):
-    def __init__(self, d_model, num_heads, d_ff, dropout):
-        super().__init__()
-        self.self_attn = MultiHeadAttention(d_model, num_heads)
-        self.cross_attn = MultiHeadAttention(d_model, num_heads)
-        self.feed_forward = FeedForward(d_model, d_ff, dropout)
-        self.norm1 = LayerNormalization(d_model)
-        self.norm2 = LayerNormalization(d_model)
-        self.norm3 = LayerNormalization(d_model)
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(DecoderLayer, self).__init__()
+        self.self_attention = MultiHeadAttention(embed_size, heads)
+        self.norm1 = LayerNorm(embed_size)
+        self.norm2 = LayerNorm(embed_size)
+        self.norm3 = LayerNorm(embed_size)
+        self.feed_forward = FeedForward(embed_size, forward_expansion)
         self.dropout = nn.Dropout(dropout)
+        self.encoder_attention = MultiHeadAttention(embed_size, heads)
 
-    def forward(self, x, enc_output, src_mask, tgt_mask):
-        x = self.norm1(x + self.dropout(self.self_attn(x, x, x, tgt_mask)))
-        x = self.norm2(x + self.dropout(self.cross_attn(x, enc_output, enc_output, src_mask)))
-        x = self.norm3(x + self.dropout(self.feed_forward(x)))
-        return x
+    def forward(self, x, value, key, src_mask, trg_mask):
+        attention = self.self_attention(x, x, x, trg_mask)
+        query = self.norm1(attention + x)
+        attention = self.encoder_attention(query, key, value, src_mask)
+        x = self.norm2(attention + query)
+        forward = self.feed_forward(x)
+        out = self.norm3(forward + x)
+        return out
